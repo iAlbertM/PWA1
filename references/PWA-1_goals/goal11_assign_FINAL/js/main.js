f09d97b771db53a3db1d53ca2475d5d54443c158
Albert Martinez <albertm@fullsail.edu>
2015-08-25 14:52:53 -0700

    updated css semantics

--- references/PWA-1_goals/goal11_assign_FINAL/js/main.js
+++ references/PWA-1_goals/goal11_assign_FINAL/js/main.js
@@ -4,46 +4,46 @@ Date: 10/26/14
 Assignment: Mid Terms for PWA-1
 */
 
+
 // self-executing function
-(function(){
+$(function(){
 	// declare global variables
 	var i = 0;
-	var button = document.querySelector('a.buttonred');
-	var form = document.getElementById('output');
-	var studentName 	= document.querySelector('name p');
-	var studentAddress  = document.getElementById('address p');
-	var studentGPA 		= document.getElementById('gpa p');
+		var sbutton = $('input');
+		var sName 	 = $('#name');
+		var sAddress = $('#address');
+		var sgpa 	 = $('#gpa');
+		var sgpaAvg  = $('gpaAvg');
 
 	// ---------------- ARRAY OF STUDENT OBJECTS
 	
 	var students = [{
-		// STUDENT 1 ///////////////////
-		// 3 main properties name, address, and gpa
-		name: "Albertini Martini" ,
+		// 3 main properties for ea. student name, address, and gpa
+		// STUDENT 1 
+		name: "Albertini Martini",
+		// address has 3 sub properties: street, city, state
 		address: {
-			// with 3 sub properties of address: street, city, state
-			street: "1841 N Fuller Ave" ,
+			street: "7777 Hollywood Blvd Apt 209",
 			city: "Los Angeles",
-			state: "Ca"},
-		// array of GPA scores for the student in this object	
-		GPA: [3.0, 4.0, 4.0]},
-
-		{
-		// STUDENT 2 ///////////////////
-		// 3 main properties name, address, and gpa
-		name: "Mattronic Tonic" ,
+			state: "Ca" 
+		},
+		// gpa scores are stored in an array
+		gpa: [3.0, 4.0, 4.0]
+	},
+	{	// STUDENT 2
+		name: "Mattronic Tonic",
 		address: {
-			// with 3 sub properties
-			street: "1841 N Fuller Ave" ,
+			street: "7777 Hollywood Blvd Apt 209",
 			city: "Los Angeles",
-			state: "Ca"},
-		// array of GPA scores for the student in this object	
-		GPA: [3.6, 4.0, 4.0]
+			state: "Ca"
+	},
+		gpa: [3.6, 4.0, 4.0]
 	}];
+
 	// // print to console the starting 2 student objects from the students array
-	// console.log("---------- Student objects in the student array ----------");
-	// console.log('1. Name: ' + students[0].name + '\n2. Address: ' + (students[0].address.street + " " + students[0].address.city + " " + students[0].address.state) + '\n3. GPA: ' + students[0].GPA);
-	// console.log('1. Name: ' + students[1].name + '\n2. Address: ' + (students[1].address.street + " " + students[1].address.city + " " + students[1].address.state) + '\n3. GPA: ' + students[1].GPA);
+	console.log("---------- Student objects in the student array ----------");
+	console.log("1. Name: " + students[0].name + "\n2. Address: " + (students[0].address.street + " " + students[0].address.city + " " + students[0].address.state) + "\n3. gpa: " + students[0].gpa);
+	console.log("1. Name: " + students[1].name + "\n2. Address: " + (students[1].address.street + " " + students[1].address.city + " " + students[1].address.state) + "\n3. gpa: " + students[1].gpa);
 
 
 
@@ -51,41 +51,44 @@ Assignment: Mid Terms for PWA-1
 
 	// create a new students object with a function
 	function addStudent(sname, sstreet, scity, sstate, sgpa) {
-		students.splice(students.length, 0, {"name": sname, "address": {
+		students.splice(students.length, 0, {
+			"name": sname,
+			"address": {
 			"street": sstreet,
 			"city": scity,
-			"state": sstate},	
-			"GPA": sgpa
-		});
-		// say hello to the new student
-		console.log("--------**** New Student has joined, Let's Welcome: " + students[students.length - 1].name + " ****--------\n");
+			"state": sstate
+		},	
+			"gpa": sgpa
+		})
 	};
+		// say hello to the new student
+		// console.log("--------**** New Student has joined, Let's Welcome: " + students[students.length - 1].name + " ****--------\n");
 
 
 	// ------------------  ADD NEW STUDENT TO THE STUDENTS ARRAY
 	// add a new student object to the student array
 	addStudent("Lilly Sosilly", "1841 N Fuller Ave", "Los Angeles", "Ca", [4.0, 4.0, 4.0]);
 
-	console.log("---------- Student objects in the student array ----------");
-	console.log('1. Name: ' + students[0].name + '\n2. Address: ' + (students[0].address.street + " " + students[0].address.city + " " + students[0].address.state) + '\n3. GPA: ' + students[0].GPA);
-	console.log('1. Name: ' + students[1].name + '\n2. Address: ' + (students[1].address.street + " " + students[1].address.city + " " + students[1].address.state) + '\n3. GPA: ' + students[1].GPA);
-	console.log('1. Name: ' + students[2].name + '\n2. Address: ' + (students[2].address.street + " " + students[2].address.city + " " + students[2].address.state) + '\n3. GPA: ' + students[2].GPA);
+	// console.log("---------- Student objects in the student array ----------");
+	// console.log("1. Name: " + students[0].name + "\n2. Address: " + (students[0].address.street + " " + students[0].address.city + " " + students[0].address.state) + "\n3. gpa: " + students[0].gpa);
+	// console.log("1. Name: " + students[1].name + "\n2. Address: " + (students[1].address.street + " " + students[1].address.city + " " + students[1].address.state) + "\n3. gpa: " + students[1].gpa);
+	// console.log("1. Name: " + students[2].name + "\n2. Address: " + (students[2].address.street + " " + students[2].address.city + " " + students[2].address.state) + "\n3. gpa: " + students[2].gpa);
 	
 	
 	
-	function showResults(){	
-		// local variables that access the DOM
+	function showResults() {	
 		var studentData;
-
-
-		if(i < students.length) {
+		// local variables that access the DOM
+		if (i < students.length) {
+			i++;
 			studentData = students[i];
-			studentName.innerHTML 	 = studentData.name;
-			studentAddress.innerHTML = studentData.address[0] + " " + studentData.address[1] +  " " + studentData.address[2];
-			studentGPA.innerHTML 	 = studentData.GPA;
-			studentGPAavg.innerHTML  = ((students[i].GPA[0] + students["i"].GPA[1] + students["i"].GPA[2]) / students["i"].GPA.length);
-		}
-
-	};
-button.onclick = function(){showResults()};
+			sName.innerHTML = studentData.name;
+			sAddress.innerHTML = ("<p>" + studentData.address.street + "<br>" + studentData.address.city +  "<br> " + studentData.address.state + "</p>");
+			sgpa.innerHTML = studentData.gpa;
+			sgpaAvg.innerHTML = (studentData.gpa[0] + studentData.gpa[1] + studentData.gpa[2]) / studentData.gpa.length;
+		};
+	}
+	console.log(sbutton);
+	console.log(showResults());
+		/* Act on the event */
 })();
